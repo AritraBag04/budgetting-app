9c8b373d1edf2985f05e0ddeaaa19ef28de45d29
AritraBag04 <aritrabag04@gmail.com>
2026-03-21 01:56:21 +0530

    Implement payment service

---
+++ src/main/java/org/liquidator/budgettingapp/service/PaymentService.java
@@ -0,0 +1,30 @@
+package org.liquidator.budgettingapp.service;
+
+import io.grpc.stub.StreamObserver;
+import org.liquidator.budgettingapp.payment.StorePaymentRequest;
+import org.liquidator.budgettingapp.payment.StorePaymentResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.liquidator.budgettingapp.payment.PaymentServiceGrpc;
+
+@Service
+public class PaymentService extends PaymentServiceGrpc.PaymentServiceImplBase {
+    private static final Logger LOG = LoggerFactory.getLogger(PaymentService.class);
+
+    @Override
+    public void storePayment(StorePaymentRequest storePaymentRequest,
+                             StreamObserver<StorePaymentResponse> streamObserver) {
+        String requestId = storePaymentRequest.getId();
+        LOG.info("Received request from client to store payment: {}", requestId);
+
+        var storePaymentResponse = StorePaymentResponse
+                .newBuilder()
+                .setId(requestId)
+                .setMessage("Successfully stored payment")
+                .build();
+
+        streamObserver.onNext(storePaymentResponse);
+        streamObserver.onCompleted();
+    }
+}
